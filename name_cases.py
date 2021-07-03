47191d465c5d2d6bac4cd18ae2cb1ad6fc94b156
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-03 18:20:27 +0200

    Упражнение 2.3. Личное сообщение

---
+++ name_cases.py
@@ -0,0 +1,5 @@
+first_name = "Vladimir"
+print(f"Hello {first_name}, would you like to learn some Python today?")
+# 2.3. Личное сообщение: сохраните имя пользователя в переменной и выведите
+# сообщение, предназначенное для конкретного человека. Сообщение должно быть
+# простым — например, «Hello Eric, would you like to learn some Python today?”.
