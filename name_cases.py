002ec496fcd7578218b81df0455e070b58d9dcb4
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-03 18:27:35 +0200

    Упражнение 2.4. Регистр символов в именах

--- name_cases.py
+++ name_cases.py
@@ -1,5 +1,7 @@
 first_name = "Vladimir"
-print(f"Hello {first_name}, would you like to learn some Python today?")
-# 2.3. Личное сообщение: сохраните имя пользователя в переменной и выведите
-# сообщение, предназначенное для конкретного человека. Сообщение должно быть
-# простым — например, «Hello Eric, would you like to learn some Python today?”.
+print(f"Hello {first_name.lower()}, would you like to learn some Python today?")
+print(f"Hello {first_name.upper()}, would you like to learn some Python today?")
+print(f"Hello {first_name.title()}, would you like to learn some Python today?")
+# 2.4. Регистр символов в именах: сохраните имя пользователя в переменной и
+# выведите его в нижнем регистре, в верхнем регистре и с капитализацией началь-
+# ных букв каждого слова.
