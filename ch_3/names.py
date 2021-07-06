849c93aecf9599b0611581208b78e8f92da66ee9
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-06 23:10:14 +0200

    Упражнение 3.1. Список друзей, вывод каждого элемента списка

---
+++ ch_3/names.py
@@ -0,0 +1,8 @@
+# 3.1. Имена: сохраните имена нескольких своих друзей в списке с именем names.
+# Выведите имя каждого друга, обратившись к каждому элементу списка (по одному
+# за раз).
+names = ['Andrey', 'Alexander', 'Stanislav', 'Konstantin']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
