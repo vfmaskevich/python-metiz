8c19b6b4450f6137f1e33abe1c2a4cb39082cb86
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-06 23:19:53 +0200

    Упражнение 3.2. Вывод сообщения с каждым элеменом списка

---
+++ ch_3/messages.py
@@ -0,0 +1,13 @@
+# 3.2. Сообщения: начните со списка, использованного в упражнении 3.1, но
+# вместо вывода имени каждого человека выведите сообщение. Основной текст всех
+# сообщений должен быть одинаковым, но каждое сообщение должно включать имя
+# адресата.
+names = ['Andrey', 'Alexander', 'Stanislav', 'Konstantin']
+message = f"Hello, {names[0]}! How are you?"
+print(message)
+message = f"Hello, {names[1]}! How are you?"
+print(message)
+message = f"Hello, {names[2]}! How are you?"
+print(message)
+message = f"Hello, {names[3]}! How are you?"
+print(message)
