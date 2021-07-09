282843f60de3b6b046527cc6ca214a65b6496118
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-09 12:32:41 +0200

    Втавка элементов в список

--- ch_3/motorcycles.py
+++ ch_3/motorcycles.py
@@ -1,21 +1,6 @@
 # # # Изменение, добавление и удаление элементов
 
-# motorcycles = ['honda', 'yamaha', 'suzuki']
-# print(motorcycles)
-
-# # Изменение элементов в списке
-# motorcycles[0] = 'ducati'
-# print(motorcycles)
-
-# # Присоединение элементов в конец списка
-# motorcycles.append('ducati')
-# print(motorcycles)
-
-# Создание списка
-motorcycles = []
-
-motorcycles.append('honda')
-motorcycles.append('yamaha')
-motorcycles.append('suzuki')
-
+# Втавка элементов в список
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
 print(motorcycles)
