9f0daff8b7bf3ef88c0c7b070766aa5867c7ccd7
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-09 12:10:23 +0200

    Изменение элементов в списке

---
+++ ch_3/motorcycles.py
@@ -0,0 +1,7 @@
+# Изменение, добавление и удаление элементов
+# Изменение элементов в списке
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
