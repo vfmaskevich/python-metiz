7528dc73a0243e63ac9e5c0faeb6fdaf999dfe52
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-29 21:48:25 +0200

    Упражнение 3.4. Список гостей. Вывод пригласительного сообщения

---
+++ ch_3/ex_3.4.py
@@ -0,0 +1,12 @@
+# 3.4. Список гостей: если бы вы могли пригласить кого угодно (из живых или
+# умерших) на обед, то кого бы вы пригласили? Создайте список, включающий
+# минимум трех людей, которых вам хотелось бы пригласить на обед. Затем
+# используйте этот список для вывода пригласительного сообщения каждому
+# участнику.
+guests = ['Andrey', 'Alexander', 'Stanislav']
+message = f"Hello, {guests[0]}! You are invited to stay for lunch, if you like."
+print(message)
+message = f"Hello, {guests[1]}! You are invited to stay for lunch, if you like."
+print(message)
+message = f"Hello, {guests[2]}! You are invited to stay for lunch, if you like."
+print(message)
