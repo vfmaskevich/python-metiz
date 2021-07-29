6fd6ef6572404e489655e480f499cb10147610e1
Vladimir Maskevich <vfmaskevich@gmail.com>
2021-07-30 00:06:14 +0200

    Упорядочение списка. Постоянная сортировка списка методом sort() (обратный порядок)

--- ch_3/cars.py
+++ ch_3/cars.py
@@ -2,3 +2,7 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
 print(cars)
+
+# В следующем примере список сортируется в порядке, обратном алфавитному
+cars.sort(reverse=True)
+print(cars)
